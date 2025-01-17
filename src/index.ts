172b5023864067b5087641fea31862e684e38e7c
Eric Crane <eric.crane10@gmail.com>
2025-01-17 09:54:17 -0800

    pseudocoding some additional workflow needs

--- src/index.ts
+++ src/index.ts
@@ -7,47 +7,54 @@ import { FlatfileRecord, bulkRecordHook } from "@flatfile/plugin-record-hook";
 import { validateEmail } from "@flatfile/plugin-validate-email";
 
 export default function (listener: FlatfileListener) {
-
-  listener.use(configureSpace({
-    workbooks: [
-      {
-        name: "TrustLayer Workbook",
-        sheets: [projects_sheet, parties_sheet],
-        actions: [
-          {
-            operation: "submitActionBg",
-            mode: "background",
-            label: "Submit",
-            primary: true,
+  listener.use(
+    configureSpace({
+      workbooks: [
+        {
+          name: "TrustLayer Workbook",
+          sheets: [projects_sheet, parties_sheet],
+          actions: [
+            {
+              operation: "submitActionBg",
+              mode: "background",
+              label: "Submit",
+              primary: true,
+            },
+          ],
+        },
+      ],
+      space: {
+        metadata: {
+          theme: {
+            root: {
+              primaryColor: "#2064dc",
+              actionColor: "#2064dc",
+            },
+            sidebar: {
+              logo: "https://trustlayer.io/_next/image?url=https%3A%2F%2Ftrustlayer-website.nyc3.digitaloceanspaces.com%2Fmedia%2F12b77db215216b09db0c1d70d00216d3.png&w=3840&q=75",
+            },
           },
-        ],
-      }
-    ],
-    space: {
-      metadata: {
-        theme: {
-          root: {
-            primaryColor: "#2064dc",
-            actionColor: "#2064dc"
-          },
-          sidebar: {
-            logo: "https://trustlayer.io/_next/image?url=https%3A%2F%2Ftrustlayer-website.nyc3.digitaloceanspaces.com%2Fmedia%2F12b77db215216b09db0c1d70d00216d3.png&w=3840&q=75"
-          },
-        }
-      }
-    }
-  }));
+        },
+      },
+    })
+  );
 
-  listener.use(jobHandler("workbook:submitActionBg", async (event, tick) => {console.log(event)}))
+  listener.use(
+    jobHandler("workbook:submitActionBg", async (event, tick) => {
+      console.log(event);
+    })
+  );
 
-  listener.use(autoFix)
+  listener.use(autoFix);
 
-  listener.use(validateEmail({
-    emailFields: ['contact_person_email'],
-    errorMessages: {
-      invalid: 'The email address is not valid'
-    }
-  }));
+  listener.use(
+    validateEmail({
+      emailFields: ["contact_person_email"],
+      errorMessages: {
+        invalid: "The email address is not valid",
+      },
+    })
+  );
 
   listener.use(
     bulkRecordHook("projects_sheet", (records: FlatfileRecord[]) => {
@@ -58,18 +65,18 @@ export default function (listener: FlatfileListener) {
         const dateRegex = /(0\d{1}|1[0-2])\/([0-2]\d{1}|3[0-1])\/(19|20)\d{2}/;
 
         if (startDate) {
-            if (!dateRegex.test(startDate)) {
-                record.addError("start_date", `Dates must be in MM/DD/YYYY format`);
-            }
+          if (!dateRegex.test(startDate)) {
+            record.addError("start_date", `Dates must be in MM/DD/YYYY format`);
+          }
         }
         if (endDate) {
-            if (!dateRegex.test(endDate)) {
-                record.addError("end_date", `Dates must be in MM/DD/YYYY format`);
-            }
+          if (!dateRegex.test(endDate)) {
+            record.addError("end_date", `Dates must be in MM/DD/YYYY format`);
+          }
         }
 
         return record;
-      })
+      });
     })
   );
 
@@ -81,17 +88,37 @@ export default function (listener: FlatfileListener) {
         const phoneRegex = /\(\d{3}\)\s\d{3}-\d{4}/;
 
         if (phone) {
-            // validate phone number with area code
-            if (!phoneRegex.test(phone)) {
-              record.addError(
-                "contact_person_phone",
-                "Phone number must be in the format (###) ###-####",
-              );
-            }
+          // validate phone number with area code
+          if (!phoneRegex.test(phone)) {
+            record.addError(
+              "contact_person_phone",
+              "Phone number must be in the format (###) ###-####"
+            );
           }
+        }
 
         return record;
-      })
+      });
     })
   );
+
+  // listener.use(checkCompliance)
+  // -- grab the currently filtered records
+  // -- stream to checking service? generate a file?
+
+  // listener.use(tagAtMapping)
+  // -- listen for mapping job
+  // -- when mapping job is complete, add metadata to record that has file name
+  // -- { sourceFileName: things.csv }
+
+  // listener.use(customdedupe)
+  // -- when mapping job is complete and file tagging is complete
+  // -- compare keys and metadata on files to determine duplicates
+  // -- follow algorithm to either auto-delete dupes or flag dupes as error
+
+  // listener.use(egress)
+  // -- grab all of the records on the sheet in flatfile
+  // -- push to your API (pass in auth from secrets)
+  // -- return a callback to Flatfile
+  // -- update metadata on the record to say "loaded"
 }
